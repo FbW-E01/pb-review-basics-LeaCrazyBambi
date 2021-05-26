f8577557a91cf183a37b4329aab6a019f947056d
LeaCrazyBambi <lheynitz@gmail.com>
2021-05-26 13:32:08 +0200

    :D

---
+++ index.js
@@ -0,0 +1,13 @@
+//  1.
+const var1 = 10;
+const var2 = -9;
+const var3 = "Something";
+const var4 = "More something";
+const var5 = `I need ${var1} beers`;
+let var6 = ` `;
+let var7 = true;
+const var8 = false;
+
+console.log(typeof var1);
+console.log(typeof var3);
+console.log(typeof var7);
